0bca1afc8e4be570c2b25ab4cd5acb1e3a71993b
TinyRedBird <149476621+TinyRedBird@users.noreply.github.com>
2026-03-27 14:47:35 +0800

    配置Redis信息and测试Redis存取数据

--- manager.py
+++ manager.py
@@ -6,22 +6,34 @@ session配置
 """
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from redis import StrictRedis
 app = Flask(__name__)
 #设置配置信息
 class Config(object):
     #调试信息
     DEBUG = True
+
     #数据库配置信息
     SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:123456@localhost:3306/info36"
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+    #Redis配置信息
+    REDIS_HOST="127.0.0.1"
+    REDIS_PORT=6379
+
 app.config.from_object(Config)
 
 #创建SQLAlchemy对象关联app
 db=SQLAlchemy(app)
 
+#创建redis对象
+redis_store= StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT,decode_responses=True)
+
 @app.route('/')
 def hello_world():
+    #测试redis存取数据
+    redis_store.set('name','damin')
+    print(redis_store.get('name'))
     return 'Hello World!'
 
 if __name__ == '__main__':
