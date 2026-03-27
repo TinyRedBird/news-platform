298ee1188074112d6b968908db25eeac300038fd
TinyRedBird <149476621+TinyRedBird@users.noreply.github.com>
2026-03-27 19:43:46 +0800

    抽取首页视图到index包

--- manager.py
+++ manager.py
@@ -8,19 +8,7 @@ session配置
 from flask import Flask, session
 from info import create_app
 
-
 app=create_app('develop')
-
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    # #测试redis存取数据
-    # redis_store.set('name','damin')
-    # print(redis_store.get('name'))
-    #
-    # #测试session存取
-    # session['name'] = 'tom'
-    # print(session.get('name'))
-    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run()
