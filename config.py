57ca50e35252962e32e04217f2c5543b05953b28
TinyRedBird <149476621+TinyRedBird@users.noreply.github.com>
2026-03-27 15:58:17 +0800

    添加config文件and抽取配置信息

---
+++ config.py
@@ -0,0 +1,41 @@
+from redis import StrictRedis
+from datetime import timedelta
+
+#设置配置信息
+class Config(object):
+    #调试信息
+    DEBUG = True
+    SECRET_KEY = '1jn242HB1HJ8BHJVhn55hj'
+
+    #数据库配置信息
+    SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:123456@localhost:3306/info36"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+    #Redis配置信息
+    REDIS_HOST="127.0.0.1"
+    REDIS_PORT=6379
+
+    #Session配置信息
+    SESSION_TYPE = 'redis'
+    SESSION_REDIS=StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    SESSION_USE_SIGNER=True #设置签名存储
+    PERMANENT_SESSION_LIFETIME=timedelta(seconds=3)
+
+#开发环境配置信息
+class DevelopConfig(Config):
+    pass
+
+#生产（线上）环境配置信息
+class ProductConfig(Config):
+    DEBUG = False
+
+#测试环境配置信息
+class TestConfig(Config):
+    pass
+
+#提供一个同一的访问入口
+config={
+    'develop':DevelopConfig,
+    'product':ProductConfig,
+    'test':TestConfig
+}
--- manager.py
+++ manager.py
@@ -4,8 +4,7 @@
 redis配置
 session配置
 """
-
-from datetime import timedelta
+from config import Config
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
@@ -13,25 +12,6 @@ from flask_session import Session
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-#设置配置信息
-class Config(object):
-    #调试信息
-    DEBUG = True
-    SECRET_KEY = '1jn242HB1HJ8BHJVhn55hj'
-
-    #数据库配置信息
-    SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:123456@localhost:3306/info36"
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-
-    #Redis配置信息
-    REDIS_HOST="127.0.0.1"
-    REDIS_PORT=6379
-
-    #Session配置信息
-    SESSION_TYPE = 'redis'
-    SESSION_REDIS=StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
-    SESSION_USE_SIGNER=True #设置签名存储
-    PERMANENT_SESSION_LIFETIME=timedelta(seconds=3)
 
 app.config.from_object(Config)
 
