21e302e6665935d71af61ab13cd36c80e3ae3f51
TinyRedBird <149476621+TinyRedBird@users.noreply.github.com>
2026-03-30 19:28:49 +0800

    favicon.ico图片返回

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -2,12 +2,13 @@ import logging
 
 from info.modules.index import index_blue
 from info import redis_store
-from flask import session
+from flask import session, render_template, current_app
+
 
 @index_blue.route('/', methods=['GET', 'POST'])
 def hello_world():
     #测试redis存取数据
-    redis_store.set('name','damin')
+    # redis_store.set('name','damin')
 
     #测试session存取
     session['name'] = 'tom'
@@ -17,4 +18,10 @@ def hello_world():
     logging.warning('hello world')
     logging.error('hello world')
 
-    return 'Hello World!'
+    return render_template('index.html')
+
+#处理网站logo接口
+@index_blue.route('/favicon.ico')
+def favicon():
+    #在flask中需要current_app.send_static_file(文件名)会自动去static下找
+    return current_app.send_static_file('favicon.ico')
