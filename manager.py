380bd21564c9af9f49fe276c97c4177e975a4934
TinyRedBird <149476621+TinyRedBird@users.noreply.github.com>
2026-04-02 21:01:56 +0800

    删除测试session

--- manager.py
+++ manager.py
@@ -1,26 +1,9 @@
-"""
-相关配置
-数据库配置
-redis配置
-session配置
-"""
+
 from info import create_app, db
 from flask_migrate import Migrate
-
+import sqlalchemy
 app=create_app('develop')
-import werkzeug.http
-original_dump_cookie = werkzeug.http.dump_cookie
 
-def patched_dump_cookie(*args, **kwargs):
-    # 打印所有参数的类型
-    print("[DEBUG] dump_cookie called with:")
-    for i, arg in enumerate(args):
-        print(f"  arg[{i}]: type={type(arg)}, value={arg}")
-    for key, val in kwargs.items():
-        print(f"  kwarg[{key}]: type={type(val)}, value={val}")
-    return original_dump_cookie(*args, **kwargs)
-
-werkzeug.http.dump_cookie = patched_dump_cookie
 #创建Manager对象管理app
 migrate = Migrate(app, db)
 
